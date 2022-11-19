f671ad9147507be2f0b32f44caa4f545d5ac7398
SemchaAI <76156008+SemchaAI@users.noreply.github.com>
2022-11-19 15:55:19 +0200

    +context
    add/added logic context

--- src/contexts/btn.js
+++ src/contexts/btn.js
@@ -3,12 +3,15 @@ import React, { useState, createContext } from "react";
 export const BtnContext = createContext({
   active: false,
   btnActive: (act) => {},
+  id: undefined,
+  setId: (act) => {},
 });
 
 const BtnContextProvider = (props) => {
   const [active, btnActive] = useState(false);
+  const [id, setId] = useState(undefined);
   return (
-    <BtnContext.Provider value={{ active, btnActive }}>
+    <BtnContext.Provider value={{ id, setId, active, btnActive }}>
       {props.children}
     </BtnContext.Provider>
   );
