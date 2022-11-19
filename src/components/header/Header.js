7b376c9c7e1cd94522f06d7c803fe732627d078d
SemchaAI <76156008+SemchaAI@users.noreply.github.com>
2022-11-19 15:52:48 +0200

    +animation+links
    was added simple animation off adding product to cart.
    added 3 links( to favor list, cart, home page)

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,35 +3,70 @@ import { ReactComponent as Favorite } from "../../img/favorite.svg";
 import { ReactComponent as User } from "../../img/user.svg";
 import logoPng from "../../img/logo.png";
 import { Link } from "react-router-dom";
+import { useContext, useEffect, useRef } from "react";
+import { CFlagBtnContext } from "../../contexts/cFlag";
+import useStore from "../../hooks/useStore";
+import styles from "./header.module.css";
+
+import cn from "classnames";
+import { useState } from "react";
 
 const Header = () => {
+  let [cartStore] = useStore("cart");
+  const { cnt, setCnt } = useContext(CFlagBtnContext);
+  useEffect(() => {
+    let test = cartStore.cart.length;
+    setCnt(test);
+  }, []);
+  console.log(cnt);
+  let [anima, setAnima] = useState(false);
+  useEffect(() => {
+    let timer1 = setTimeout(() => setAnima(true), 300);
+    setAnima(false);
+    return () => {
+      clearTimeout(timer1);
+    };
+  }, [cnt]);
   return (
     <header className="container">
-      <div className="leftBar">
-        <img
-          width={40}
-          height={40}
-          // src={require("./img/logo.png")}
-          src={logoPng}
-          alt="sneakers logo"
-        />
-        <div className="siteInfo">
-          <h2>React sneakers</h2>
-          <p>Магазин лучших кроссовок</p>
+      <Link to="/">
+        <div className="leftBar">
+          <img
+            width={40}
+            height={40}
+            // src={require("./img/logo.png")}
+            src={logoPng}
+            alt="sneakers logo"
+          />
+          <div className="siteInfo">
+            <h2>React sneakers</h2>
+            <p>Магазин лучших кроссовок</p>
+          </div>
         </div>
-      </div>
+      </Link>
       <ul className="rightBar">
         <li className="cartPrice">
           <Link to="/cart">
             <Cart />
           </Link>
-          <p>1205 руб.</p>
+          <p
+            className={cn({
+              [styles.counter]: true,
+              [styles.animation]: anima === true,
+            })}
+          >
+            {cnt}
+          </p>
         </li>
         <li className="favorite">
-          <Favorite />
+          <Link to="/favorite">
+            <Favorite />
+          </Link>
         </li>
         <li>
-          <User />
+          <Link to="/user">
+            <User />
+          </Link>
         </li>
       </ul>
     </header>
