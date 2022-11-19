c6a13c9c74cc895f4917749ddd9e932b46661fbe
SemchaAI <76156008+SemchaAI@users.noreply.github.com>
2022-11-19 15:31:37 +0200

    -del auth +added context
    auth option moved to index.js

--- src/App.js
+++ src/App.js
@@ -7,45 +7,21 @@ import { observer } from "mobx-react-lite";
 import RouterView from "./routes";
 import Header from "./components/header/Header";
 import BtnContextProvider from "./contexts/btn";
-
-import { add, testChange, addInit, load } from "./api/apiCart";
-
-import { getAuth, signInAnonymously } from "firebase/auth";
-import app from "./api/apiCard";
-import firebase from "firebase/compat/app";
-import Cart from "./store/cart";
-
-firebase.auth(app).onAuthStateChanged(function (user) {
-  if (user) {
-    console.log("signed");
-    localStorage.setItem("user", user.uid);
-    if (Cart) {
-    }
-    console.log(addInit({ [user.uid]: [] }));
-    //console.log(add({ id: 1, cnt: 1 }));
-    //console.log(testChange());
-    console.log(user.uid);
-  } else {
-    firebase
-      .auth()
-      .signInAnonymously()
-      .then(localStorage.setItem("user", user.uid));
-    // console.log(add({ [user.uid]: [] }));
-    console.log(user.uid);
-  }
-});
+import CFlagContextProvider from "./contexts/cFlag";
 
 function App() {
   return (
-    <BtnContextProvider>
-      <div className="wrapper">
-        <Header />
-        <main>
-          <RouterView />
-        </main>
-        <footer></footer>
-      </div>
-    </BtnContextProvider>
+    <CFlagContextProvider>
+      <BtnContextProvider>
+        <div className="wrapper">
+          <Header />
+          <main>
+            <RouterView />
+          </main>
+          <footer></footer>
+        </div>
+      </BtnContextProvider>
+    </CFlagContextProvider>
   );
 }
 
